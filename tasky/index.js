9210327795d16589438455e8278f427525c4ccf3
manishsaraan <manish021js@gmail.com>
2018-10-15 14:29:35 +0530

    hide app on blur

--- tasky/index.js
+++ tasky/index.js
@@ -15,6 +15,7 @@ app.on("ready", () => {
     });
 
     mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+    mainWindow.on("blur", () =>  mainWindow.hide());
     
     const iconName = process.platform === "win32" ? "windows-icon.png" : "iconTemplate@2x.png";
     const iconPath = path.join(__dirname + `/src/assets/${iconName}`);
