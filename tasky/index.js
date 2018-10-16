d5ba83d6019fc78c32adbf7b5fead18879981589
manishsaraan <manish021js@gmail.com>
2018-10-16 14:43:23 +0530

    code refactor

--- tasky/index.js
+++ tasky/index.js
@@ -2,20 +2,13 @@ const electron = require("electron");
 const path  = require("path");
 const { app, BrowserWindow } = electron;
 const TimerTray = require('./app/time_tray');
+const MainWindow = require("./app/main_window");
 
 let mainWindow, tray;
 
 app.on("ready", () => {
-    mainWindow = new BrowserWindow({
-        height: 470,
-        width: 300,
-        frame: false,
-        resizable: false,
-        show: false,
-    });
 
-    mainWindow.loadURL(`file://${__dirname}/src/index.html`);
-    mainWindow.on("blur", () =>  mainWindow.hide());
+    mainWindow = new MainWindow(`file://${__dirname}/src/index.html`);
     
     const iconName = process.platform === "win32" ? "windows-icon.png" : "iconTemplate@2x.png";
     const iconPath = path.join(__dirname + `/src/assets/${iconName}`);
