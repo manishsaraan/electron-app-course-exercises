a559a4e06f0e0306855a7e1c16ff40448821705e
manishsaraan <manish021js@gmail.com>
2018-10-16 19:26:37 +0530

    timer added in tray

--- tasky/index.js
+++ tasky/index.js
@@ -1,6 +1,6 @@
 const electron = require("electron");
 const path  = require("path");
-const { app, BrowserWindow } = electron;
+const { app, ipcMain } = electron;
 const TimerTray = require('./app/time_tray');
 const MainWindow = require("./app/main_window");
 
@@ -13,4 +13,8 @@ app.on("ready", () => {
     const iconName = process.platform === "win32" ? "windows-icon.png" : "iconTemplate@2x.png";
     const iconPath = path.join(__dirname + `/src/assets/${iconName}`);
     tray = new TimerTray(iconPath, mainWindow);
+});
+
+ipcMain.on("update-timer", (event, timeLeft) => {
+    tray.setTitle(timeLeft);
 });
